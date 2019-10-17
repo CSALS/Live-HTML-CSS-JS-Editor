f349e41b3e4ff8ad78c48bd1906f4aa7349a2f59
CSALS <charansaicharan14@gmail.com>
2019-10-18 03:11:58 +0530

    Embed text-editors using Ace editor

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 References -> https://tryenlight.github.io/code-editor.html
+
+Add different themes support for the editors
+
+Add clear button for each of three editors and clear all button also (use destroy() function https://ace.c9.io/#nav=api&api=editor)
+
+Add mouseover text to minimize and maximize buttons
+
+Maybe try to add local environment where users can add pictures and refer to them in html files as if the images are in the same location as the source code.
--- app.js
+++ app.js
@@ -1,14 +1,148 @@
-function compile() {
-    var html = document.getElementById("html");
-    var css = document.getElementById("css");
-    var js = document.getElementById("js");
-    var code = document.getElementById("code").contentWindow.document;
+function getUserCode() {
+    return htmlEditor.getValue() + "<style>" + cssEditor.getValue() + "</style>" + "<script>" + jsEditor.getValue() + "</script>";
+}
+function update() {
+    //this is the content of iframe
+    var code = document.getElementById('iframe').contentWindow.document;
+    code.open();
+    //getting value from editor and puts in the iframe
+    code.write(getUserCode());
+    code.close();
+}
+function loadHTMLEditor() {
+    defaultHTMLValue = "<!DOCTYPE html>\n\n<html>\n\n    <!-- Your HTML code goes right here -->\n\n</html>"
+    //tells ace editor to use editor element , window.editor makes it global in the javascript file
+    window.htmlEditor = ace.edit("htmlEditor");
+    //mode mode
+    htmlEditor.setTheme("ace/theme/dracula");
+    //html mode
+    htmlEditor.getSession().setMode("ace/mode/html");
+    //sample text
+    htmlEditor.setValue(defaultHTMLValue,1); //1 = moves cursor to end
+    // when something changed in editor update is called
+    htmlEditor.getSession().on('change', function() {
+        update();
+    });
+    // puts cursor in the editor
+    htmlEditor.focus();
     
-    document.body.onkeyup = function(){
-      code.open();
-      code.writeln(html.value+"<style>"+css.value+"</style>"+"<script>" + js.value + "</script>");
-      code.close();
-    };
-};
+    //htmlEditor.setOption('showLineNumbers', true);
+    htmlEditor.setOptions({
+        fontSize: "12.5pt",
+        showLineNumbers: true,
+        vScrollBarAlwaysVisible:false,
+        enableBasicAutocompletion: true,
+        enableSnippets: true,
+        enableLiveAutocompletion: false
+    });
 
-compile();
+    htmlEditor.setShowPrintMargin(false);
+    //htmlEditor.setBehavioursEnabled(false);
+}
+function loadCSSEditor() {
+    defaultCSSValue = "/*        Your CSS Code Goes Here           */"
+    //tells ace editor to use editor element , window.editor makes it global in the javascript file
+    window.cssEditor = ace.edit("cssEditor");
+    //mode mode
+    cssEditor.setTheme("ace/theme/dracula");
+    //html mode
+    cssEditor.getSession().setMode("ace/mode/css");
+    //sample text
+    cssEditor.setValue(defaultCSSValue,1); //1 = moves cursor to end
+    // when something changed in editor update is called
+    cssEditor.getSession().on('change', function() {
+        update();
+    });
+    // puts cursor in the editor
+    cssEditor.focus();
+
+    //htmlEditor.setOption('showLineNumbers', true);
+    cssEditor.setOptions({
+        fontSize: "12.5pt",
+        showLineNumbers: true,
+        vScrollBarAlwaysVisible:true,
+        // enableBasicAutocompletion: true,
+        // enableSnippets: true,
+        // enableLiveAutocompletion: false
+    });
+
+    cssEditor.setShowPrintMargin(false);
+    //cssEditor.setBehavioursEnabled(false);
+}
+function loadJSEditor() {
+    defaultJSValue = "/*     Your JAVASCRIPT Code Goes Here       */"
+    //tells ace editor to use editor element , window.editor makes it global in the javascript file
+    window.jsEditor = ace.edit("jsEditor");
+    //mode mode
+    jsEditor.setTheme("ace/theme/dracula");
+    //html mode
+    jsEditor.getSession().setMode("ace/mode/javascript");
+    //sample text
+    jsEditor.setValue(defaultJSValue,1); //1 = moves cursor to end
+    // when something changed in editor update is called
+    jsEditor.getSession().on('change', function() {
+        update();
+    });
+    // puts cursor in the editor
+    jsEditor.focus();
+    
+    //htmlEditor.setOption('showLineNumbers', true);
+    jsEditor.setOptions({
+        fontSize: "12.5pt",
+        showLineNumbers: true,
+        vScrollBarAlwaysVisible:true,
+        enableBasicAutocompletion: true,
+        enableSnippets: true,
+        enableLiveAutocompletion: false
+    });
+
+    jsEditor.setShowPrintMargin(false);
+    //htmlEditor.setBehavioursEnabled(false);
+}
+function setupEditor() {
+    loadHTMLEditor();
+    loadCSSEditor();
+    loadJSEditor();
+}
+function ready() {
+    setupEditor();
+}
+
+function maximizeIFrame() {
+    //First make Iframe height larger
+    let iframe = document.getElementById("iframe");
+    iframe.style.height = "98%";
+    iframe.style.width = "100%";
+    //Next equate all 3 editors dimensions to 0
+    let htmlEditor = document.getElementById("htmlEditor");
+    htmlEditor.style.height = "0%";
+    htmlEditor.style.width = "0%";
+    let cssEditor = document.getElementById("cssEditor");
+    cssEditor.style.height = "0%";
+    cssEditor.style.width = "0%";
+    let jsEditor = document.getElementById("jsEditor");
+    jsEditor.style.height = "0%";
+    jsEditor.style.width = "0%";
+    //Make editors height 5% which has labels and buttons
+    let allEditors = document.getElementById("editors");
+    allEditors.style.height = "5%";
+    allEditors.style.width = "100%";
+}
+function minimizeIframe() {
+    //Going in reverse order from maximizeFrame() to reset all elements to their original dimensions
+    var editors = document.getElementById("editors");
+    editors.style.height = "50%";
+    editors.style.width = "100%";
+    var editors = document.getElementById("htmlEditor");
+    editors.style.height = "90%";
+    editors.style.width = "32%";
+    editors = document.getElementById("cssEditor");
+    editors.style.height = "90%";
+    editors.style.width = "32%";
+    editors = document.getElementById("jsEditor");
+    editors.style.height = "90%";
+    editors.style.width = "32%";
+    var el = document.getElementById("iframe");
+    el.style.height = "50%";
+    el.style.width = "100%";
+}
